e0be96d427490ef9a956ccb63469fb407b58b05f
alihmaidi <alihmaidi095@gmail.com>
2024-08-04 22:38:04 +0300

    edit

--- Dto/obj/Debug/net7.0/Dto.AssemblyInfo.cs
+++ Dto/obj/Debug/net7.0/Dto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Dto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a0067d6c6aa29fe7657586cc6326c9e8a756dec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be491ab3c0359998471f27575e1fb5f6ed17d56")]
 [assembly: System.Reflection.AssemblyProductAttribute("Dto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Dto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
