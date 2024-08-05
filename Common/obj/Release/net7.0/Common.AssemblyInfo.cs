b79dff32366a6aded9deb0a38b5e87f53195a317
alihmaidi <alihmaidi095@gmail.com>
2024-08-05 15:10:56 +0300

    ws

--- Common/obj/Release/net7.0/Common.AssemblyInfo.cs
+++ Common/obj/Release/net7.0/Common.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91de76f40237a106ec7c647630f5887e7291b2f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+298a6fa97f238b2189e6ec3f12cf6ad36e1c106c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
